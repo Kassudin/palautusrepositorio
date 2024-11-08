c0503288d2f81c1721ea8419f26da61ad001fcd1
Kassudin <kasper.kinnarinen@live.fi>
2024-11-08 18:18:52 +0200

    Viikko 2 teht2

--- viikko2/project-reader/src/project.py
+++ viikko2/project-reader/src/project.py
@@ -1,17 +1,21 @@
 class Project:
-    def __init__(self, name, description, dependencies, dev_dependencies):
+    def __init__(self, name, description, license, authors, dependencies, dev_dependencies):
         self.name = name
         self.description = description
+        self.license = license
+        self.authors = authors
         self.dependencies = dependencies
         self.dev_dependencies = dev_dependencies
 
-    def _stringify_dependencies(self, dependencies):
-        return ", ".join(dependencies) if len(dependencies) > 0 else "-"
+    def _stringify(self, items):
+        return "\n- " + "\n- ".join(items) if items else "-"
 
     def __str__(self):
         return (
             f"Name: {self.name}"
             f"\nDescription: {self.description or '-'}"
-            f"\nDependencies: {self._stringify_dependencies(self.dependencies)}"
-            f"\nDevelopment dependencies: {self._stringify_dependencies(self.dev_dependencies)}"
+            f"\nLicense: {self.license or '-'}"
+            f"\n\nAuthors:{self._stringify(self.authors)}"
+            f"\n\nDependencies:{self._stringify(self.dependencies)}"
+            f"\n\nDevelopment dependencies:{self._stringify(self.dev_dependencies)}"
         )
--- viikko2/project-reader/src/project_reader.py
+++ viikko2/project-reader/src/project_reader.py
@@ -1,5 +1,6 @@
 from urllib import request
 from project import Project
+import toml
 
 
 class ProjectReader:
@@ -9,7 +10,22 @@ class ProjectReader:
     def get_project(self):
         # tiedoston merkkijonomuotoinen sisältö
         content = request.urlopen(self._url).read().decode("utf-8")
-        print(content)
+        #print(content)
+        #tiedonston toml sisältö:
+        parsed_toml = toml.loads(content)
+        #print ("Sisältö:\n", parsed_toml)
+
+        tool = parsed_toml.get("tool", {}).get("poetry", {})
+        name = tool.get("name")
+        description = tool.get("description")
+        license = tool.get("license")
+        authors = tool.get("authors", [])
+        dependencies = tool.get("dependencies", {})
+        dependencies_list = list(dependencies.keys())
+        dev_dependencies = tool.get("group", {}).get("dev", {}).get("dependencies", {})
+        dev_dependencies_list = list(dev_dependencies.keys())
+        
 
         # deserialisoi TOML-formaatissa oleva merkkijono ja muodosta Project-olio sen tietojen perusteella
-        return Project("Test name", "Test description", [], [])
+        return Project(name, description, license, authors, dependencies_list, dev_dependencies_list)
+        #python3 src/index.py
