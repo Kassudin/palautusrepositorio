0ca24b2d56f8c318dbe59321ec655c7948fa7549
Kassudin <kasper.kinnarinen@live.fi>
2024-11-11 14:44:54 +0200

    Viikko 2 teht. 10 ja 11

--- viikko2/nhl-reader/src/index.py
+++ viikko2/nhl-reader/src/index.py
@@ -1,7 +1,27 @@
 from player import Player
+import requests
+
 
 def main():
-    pass
+    url = "https://studies.cs.helsinki.fi/nhlstats/2023-24/players"
+    response = requests.get(url).json()
+
+    #print("JSON-muotoinen vastaus:")
+    #print(response)
+
+    players = []
+
+    for player_dict in response:
+        player = Player(player_dict)
+        players.append(player)
+
+    print("Players from FIN\n")
+
+    finnish_players = [player for player in players if player.nationality == "FIN"]
+    sorted_players = sorted(finnish_players, key=lambda p: p.goals + p.assists, reverse=True)
+
+    for player in sorted_players:
+        print(player)
 
 if __name__ == "__main__":
     main()
--- viikko2/nhl-reader/src/player.py
+++ viikko2/nhl-reader/src/player.py
@@ -1,6 +1,12 @@
 class Player:
-    def __init__(self, dict):
-        self.name = dict['name']
-    
+    def __init__(self, player_dict):
+        self.name = player_dict.get("name", "")
+        self.team = player_dict.get("team", "")
+        self.goals = int(player_dict.get("goals", 0))
+        self.assists = int(player_dict.get("assists", 0))
+        self.nationality = player_dict.get("nationality", "")
+
     def __str__(self):
-        return self.name
+        total_points = self.goals + self.assists
+        return f"{self.name:20} {self.team:3} {self.goals:2} + {self.assists:2} = {total_points:3}"
+
